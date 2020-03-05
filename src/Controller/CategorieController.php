8dc2c1c09e74dc2ebc3a87a17652e2cf9867d19d
Ipalakot <ipalakot@gmail.com>
2020-03-05 17:38:59 +0100

    Categorie suite

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -10,13 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/categorie")
- */
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/", name="categorie_index", methods={"GET"})
+     * @Route("/categorie", name="categorie_index", methods={"GET"})
      */
     public function index(CategorieRepository $categorieRepository): Response
     {
