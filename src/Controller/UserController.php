e33db46bcd2034e217016706ba7c9862ac77d508
Ipalakot <ipalakot@gmail.com>
2020-03-17 23:49:28 +0100

    User Template

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,19 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
+/**
+ * @Route("/user")
+ */
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user")
      */
+
     public function index()
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+
         ]);
     }
 
