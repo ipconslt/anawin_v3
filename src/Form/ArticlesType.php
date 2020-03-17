c131f64c21fa139f71a34b1118962169b0dda25a
Ipalakot <ipalakot@gmail.com>
2020-03-17 22:11:55 +0100

    Adding Articles Entity

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('categorie')
+            ->add('auteur')
+            ->add('resume')
+            ->add('contenu')
+            ->add('created_At')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
