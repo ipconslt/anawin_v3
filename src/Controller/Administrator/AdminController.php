a06f6593f79e6e86ceb1db5026553ed1f36b0f8c
Ipalakot <ipalakot@gmail.com>
2020-03-18 13:55:48 +0100

    Administrator

--- src/Controller/Administrator/AdminController.php
+++ src/Controller/Administrator/AdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Administrator;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
